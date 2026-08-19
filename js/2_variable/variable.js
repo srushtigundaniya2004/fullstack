a991e20944ac461af8b751366c067e02217daad8
srushti <srushtigundaniya2004@gmail.com>
2026-08-19 19:46:23 +0530

    update file

---
+++ js/2_variable/variable.js
@@ -0,0 +1,17 @@
+//alert(`hello`);
+
+//var x=10;
+//var x=15;
+//document.write(`ans:-${(x)}`);
+
+ let city="surat";
+// let city="ahemdabad"; error
+// document.write(`ans:-${(city)}`);
+
+// const pi=3.14;
+// console .log(`ans:-${pi}`);
+
+// let x="10";
+// document.write(`ans:-${(x)}`)
+
+// console.log(typeof(x));
